6d869b53a4761922364f9ccdfa7d78c93745f748
Ryuki <ryuki.sato.lol@gmail.com>
2019-11-01 01:22:54 +0900

    update for migration file postsedt

--- db/migrate/20191024102553_posts_edit.rb
+++ db/migrate/20191024102553_posts_edit.rb
@@ -1,5 +1,5 @@
 class PostsEdit < ActiveRecord::Migration[5.2]
   def change
-    change_column :posts, :zaiko, :boolean
+    change_column :posts, :zaiko, 'boolean USING CAST(zaiko AS boolean)'
   end
 end
