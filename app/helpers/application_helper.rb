5ee012e53dfbbc45cde58c062c2bb3de60026f89
Ryuki <ryuki.sato.lol@gmail.com>
2019-11-16 16:33:27 +0900

    hyperlink url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+    require "uri"
+    #URL文字列をハイパーリンク化
+ 
+    def text_url_to_link(text)
+
+        URI.extract(text, ["http", "https"]).uniq.each do |url|#http or https で始まるテキスト要素の重複削除
+          text.gsub!(url, "<a href=\"#{url}\" target=\"_blank\">#{url}</a>")#ハイパーリンク化
+        end
+        text
+      end
+
 end
