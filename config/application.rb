ee522a234628ca78fa2fee331126f62b87ba74aa
Ryuki <ryuki.sato.lol@gmail.com>
2020-01-07 09:15:02 +0900

    add middlewareuse to applicationrb

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module SampleApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
+    #2020.1.7追加
+    config.middleware.use ActionDispatch::Session::CookieStore, {:key=>"_sample_app_session", :cookie_only=>true}
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
