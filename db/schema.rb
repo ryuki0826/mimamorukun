21c4c29c6525fc018d8af8de360a5cf250001c9e
Ryu <ryuki.sato.lol@gmail.com>
2020-03-19 22:02:00 +0000

    add_movie_top

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2020_02_05_231034) do
 
-  create_table "Posts", force: :cascade do |t|
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -21,13 +28,6 @@ ActiveRecord::Schema.define(version: 2020_02_05_231034) do
     t.string "url"
     t.text "content2"
     t.string "option"
-  end
-
-  create_table "likes", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
